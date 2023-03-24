016a48fa4e24a4c3c5ebf82e4a6870e6f2c8a4b0
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-03-24 15:59:37 +0200

    Enum example.

---
+++ 03_enums/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _03_enums
+{
+    internal class Program
+    {
+        enum Menu { Open, Save, Close, SaveAs }
+
+        enum Direction : short
+        { 
+            North = 1, South, East, West, aregaer, aergarrr
+        }
+
+        static void Main(string[] args)
+        {
+            // ---------- working with enum ----------
+            // directions: North, South, East, West
+
+            Direction planA = Direction.South;
+            Direction planB = Direction.East;
+
+            Console.WriteLine($"Plan A: go {planA} - {(short)planA}");
+            Console.WriteLine($"Plan B: go {planB} - {(short)planB}");
+
+            // ---------- get enum values
+            string[] names = Enum.GetNames(typeof(Direction));
+            short[] values = (short[])Enum.GetValues(typeof(Direction));
+
+            Console.WriteLine("Choose your direction:");
+
+            foreach (Direction value in values)
+                Console.WriteLine($"{(short)value} - {value}");
+
+            string? input = Console.ReadLine();
+
+            // ---------- enum parsing
+            Direction myDir = (Direction)Enum.Parse(typeof(Direction), input);
+
+            // ---------- check if value is defined
+            if (!Enum.IsDefined(typeof(Direction), myDir))
+            {
+                Console.WriteLine("Your value is not defined!");
+            }
+
+            Console.Write("The opposite direction is ... ");
+
+            switch (myDir)
+            {
+                case Direction.North: Console.WriteLine("south"); break;
+                case Direction.South: Console.WriteLine("north"); break;
+                case Direction.East: Console.WriteLine("west"); break;
+                case Direction.West: Console.WriteLine("east"); break;
+            }
+        }
+    }
+}
