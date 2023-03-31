4682fb217717a06b939add452377afb5ff5a9bc9
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-03-31 16:25:54 +0300

    Exceptions example.

---
+++ 09_exceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _09_exceptions
+{
+    class Book
+    {
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Title can not be empty!");
+
+                if (!char.IsUpper(value[0]))
+                    throw new Exception("Title must start with upper letter!");
+
+                title = value;
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // input data
+            }
+            catch (Exception)
+            {
+                // handling
+            }
+        }
+    }
+}
