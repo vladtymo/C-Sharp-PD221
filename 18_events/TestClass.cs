9724621cac7e5c96cb660fc4729fbae18db626f0
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-05-01 16:16:33 +0300

    Custom event add/remove logic.

---
+++ 18_events/TestClass.cs
@@ -0,0 +1,31 @@
+﻿namespace _18_events
+{
+    class TestClass
+    {
+        List<Action> Actions = new List<Action>();
+
+        public event Action TestEvent
+        {
+            // subscribe [+=]
+            add
+            {
+                // additional logic
+                if (!this.Actions.Contains(value))
+                    this.Actions.Add(value);
+            }
+            // unsubscribe [-=]
+            remove
+            {
+                this.Actions.Remove(value);
+            }
+        }
+
+        public void Notify()
+        {
+            foreach (var item in Actions)
+            {
+                item?.Invoke();
+            }
+        }
+    }
+}
